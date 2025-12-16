8ad3b2f86e4fabb74300b6c840130bf15f3d91ba
MANGLE Pierre Chris Yohann <yohann.mangle@cie.ci>
2025-12-16 17:00:27 +0000

    Initial commit

---
+++ src/home.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+const Home = () => {
+  return <div className="home"></div>;
+};
+
+export default Home;
---
+++ src/navbar.js
@@ -0,0 +1,20 @@
+// const nam = () =>{
+//     return (
+//
+//   );
+// }
+
+// export default nam  ;
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="links">
+        <a href="/"> homde </a>
+        <a href="/create">new blog </a>
+        <p> ruerjdsfq</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
